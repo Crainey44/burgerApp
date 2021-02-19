99c7790503b113d7909d42d2fd919fdc7c9a2c11
charles rainey <charles.rainey@gmail.com>
2021-02-18 22:29:05 -0500

    updated orm file

--- config/orm.js
+++ config/orm.js
@@ -1,54 +1,104 @@
 const connection = require('./connection.js');
 
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = (num) => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+  const arr = [];
+
+  // Loop through the keys and push the key/value as a string int arr
+  for (const key in ob) {
+    let value = ob[key];
+    // Check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+        value = `'${value}'`;
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(`${key}=${value}`);
+    }
+  }
+
+  // Translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
 const orm = {
-    selectAll(tableInput, colToSearch, valOfCol) {
-      //update selectAll query string
-      const queryString = 'SELECT * FROM ?? WHERE ?? = ?';
+    selectAll: function(tableInput, cb) {
+      const queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(
-        queryString,
-        [tableInput, colToSearch, valOfCol],
-        (err, result) => {
-          if (err) throw err;
+        queryString, function(err, result) {
+          if (err) {
+            throw err;
           console.log(result);
         }
-      );
+        cb(result);
+        });
     },
-    insertOne(whatToSelect, table, orderCol) {
-      //update insertOne query string
-      const queryString = 'SELECT ?? FROM ?? ORDER BY ?? DESC';
-      console.log(queryString);
-      connection.query(
-        queryString,
-        [whatToSelect, table, orderCol],
-        (err, result) => {
-          if (err) throw err;
-          console.log(result);
-        }
-      );
-    },
-    updateOne(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-      //update updateOne query string
-      const queryString =
-        'SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1';
+    insertOne: function(table, cols, vals,  cb) {
+      const queryString = `INSERT INTO ${table}`;
+
+      queryString += ' (';
+      queryString += cols.toString();
+      queryString += ') ';
+      queryString += 'VALUES (';
+      queryString += printQuestionMarks(vals.length);
+      queryString += ') ';
   
-      connection.query(
-        queryString,
-        [
-          tableOneCol,
-          tableOneCol,
-          tableOne,
-          tableTwo,
-          tableTwo,
-          tableTwoForeignKey,
-          tableOne,
-          tableOneCol,
-        ],
-        (err, result) => {
-          if (err) throw err;
-          console.log(result);
+      console.log(queryString);
+  
+      connection.query(queryString, vals, (err, result) => {
+        if (err) {
+          throw err;
         }
-      );
+  
+        cb(result);
+      });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+      //update updateOne query string
+      const queryString = `UPDATE ${table}`;
+      
+      queryString += ' SET ';
+      queryString += objToSql(objColVals);
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete(table, condition, cb) {
+      let queryString = `DELETE FROM ${table}`;
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
     },
   };
   
+  // Export the orm object for the model (cat.js).
   module.exports = orm;
+  
